e5401763c5c67c92d999f010ba44693445b4b8bb
kichikuchi <kikuchi0111@gmail.com>
2018-03-10 01:41:54 +0900

    add prototype

---
+++ SwiftGoFDesignPattern.playground/Pages/Prototype.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ **Example**
+ */
+
+class ChungasRevengeDisplay {
+    var name: String?
+    let font: String
+
+    init(font: String) {
+        self.font = font
+    }
+
+    func clone() -> ChungasRevengeDisplay {
+        return ChungasRevengeDisplay(font: self.font)
+    }
+}
+
+/*:
+ **Usage**
+ */
+
+let Prototype = ChungasRevengeDisplay(font: "GotanProject")
+
+let Philippe = Prototype.clone()
+Philippe.name = "Philippe"
+
+let Christoph = Prototype.clone()
+Christoph.name = "Christoph"
+
+let Eduardo = Prototype.clone()
+Eduardo.name = "Eduardo"
+
+//: [Next](@next)
