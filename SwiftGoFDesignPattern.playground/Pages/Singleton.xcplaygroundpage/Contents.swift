21fb0b760ae3151f2228370bf9e2deb301c3cab4
kichikuchi <kikuchi0111@gmail.com>
2018-03-10 13:49:12 +0900

    add singleton

---
+++ SwiftGoFDesignPattern.playground/Pages/Singleton.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ **Example**
+ */
+
+class DeathStartSuperlaser {
+    static let shared = DeathStartSuperlaser()
+    private init() {}
+}
+
+/*:
+ **Usage**
+ */
+
+let laser = DeathStartSuperlaser.shared
+
+//: [Next](@next)
